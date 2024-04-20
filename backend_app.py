63a14c3b4ceb367214f51e4523af61d0ba1cf5d5
Clément M <125815203+Clemagda@users.noreply.github.com>
2024-04-20 10:39:01 +0200

    Update backend_app.py
    Fixed port path

--- backend_app.py
+++ backend_app.py
@@ -75,6 +75,6 @@ async def segment_image(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    if __name__ == "__main__":
+        port = int(os.getenv("PORT", 8000))  # Utiliser le port défini par Azure ou, par défaut, 8000
+        uvicorn.run(app, host="0.0.0.0", port=port)
